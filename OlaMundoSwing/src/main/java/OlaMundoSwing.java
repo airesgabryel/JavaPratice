493f255989c65b1493803f9894d8f9bb193d2a48
airesgabryel <56Gabryel@gmail.com>
2025-10-03 19:58:04 -0300

    criando projeto utilizando biblioteca swing

---
+++ OlaMundoSwing/src/main/java/OlaMundoSwing.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Gabry
+ */
+public class OlaMundoSwing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
